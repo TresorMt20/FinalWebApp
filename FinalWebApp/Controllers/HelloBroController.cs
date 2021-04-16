ea34077b4d70e091a25ae0c1d3a889a377d30966
Tresor mt <tresor.tshiabamuteba@yahoo.fr>
2021-04-16 17:29:06 +0300

    control

---
+++ FinalWebApp/Controllers/HelloBroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalWebApp.Controllers
+{
+    public class HelloBroController : Controller
+    {
+        public string Lol()
+        {
+            return "lol guys come on";
+        }
+    }
+}
