8f400177737c11d83ad67a498b770fd7238ed91b
Tresor mt <tresor.tshiabamuteba@yahoo.fr>
2021-04-16 15:39:22 +0300

    control

---
+++ FinalWebApp/Controllers/HelloWorldController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalWebApp.Controllers
+{
+    public class HelloWorldController1 : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+    }
+}
